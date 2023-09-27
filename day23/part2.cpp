082dc73d1f64ccc3871963408c2dc7d5ca55186b
oloturia <5429234+oloturia@users.noreply.github.com>
2023-09-27 16:45:18 +0200

    day 23

--- day23/part2.cpp
+++ day23/part2.cpp
@@ -6,14 +6,14 @@
 
 using std::cout; using std::string; using std::ifstream; using std::getline; using std::map; using std::to_string;
 
-string elab (string filename);
+unsigned long elab (string filename);
 
 struct Cup {
-	unsigned int next;
+	unsigned long next;
 };
 
 int main(int argc, char**argv) {
-	string solution;
+	unsigned long solution;
 	if (argc >= 2) {
 		solution =  elab(argv[1]);
 	} else {
@@ -22,16 +22,16 @@ int main(int argc, char**argv) {
 	cout << solution << '\n';
 }
 
-string elab(string filename) {
+unsigned long elab(string filename) {
 	ifstream file(filename);
 	string temp;
-	map<int,Cup> cups;
-	unsigned int num;
-	unsigned int index = 0;
-	unsigned int maxind = 0;
+	map<unsigned long,Cup> cups;
+	unsigned long num;
+	unsigned long index = 0;
+	unsigned long maxind = 0;
 	
 	while (getline(file,temp)){;
-		for(unsigned int i = 0; i < 9; i++) {
+		for(unsigned long i = 0; i < 9; i++) {
 			num = temp[i] - 48;
 			if (num > maxind) {
 				maxind = num;
@@ -41,44 +41,44 @@ string elab(string filename) {
 			}
 			if (i < 8) {
 				cups[num].next = temp[i+1] - 48;
-			} else {
-				cups[num].next = temp[0] - 48;
 			}
-
 		}
 	}
 	file.close();
-
-	unsigned int first;
-	unsigned int mid;
-	unsigned int last;
-	unsigned int destination;
-	unsigned int temp_d;
 	
-	for(unsigned int mv = 0; mv < 100; mv++) {		
+	cups[num].next = maxind+1;
+	for(unsigned long i = maxind+1; i < 1000000; i++) {
+		cups[i].next = i+1;
+	}
+	cups[1000000].next = index;
+	maxind = 1000000;
+	
+	unsigned long first;
+	unsigned long mid;
+	unsigned long last;
+	unsigned long destination;
+	unsigned long temp_d;
+
+	for(unsigned long mv = 0; mv < 10000000; mv++) {
 		first = cups[index].next;
 		mid = cups[first].next;
 		last = cups[mid].next;
 		cups[index].next = cups[last].next;
 
 		destination = index - 1;
-		if (destination == 0) {destination = maxind;}	
+		if (destination == 0) {destination = maxind;}
+		
 		while(destination == first || destination == mid || destination == last) {
 			destination --;
 			if (destination == 0) {destination = maxind;}
 		}
-		
-		temp_d = cups[destination].next;		
+		temp_d = cups[destination].next;
 		cups[destination].next = first;
-		cups[last].next = temp_d;	
+		cups[last].next = temp_d;
 		index = cups[index].next;
 	}
-	string solution;
-	unsigned int index_zero = 1;
-	while(cups[index_zero].next != 1) {
-		solution = solution + to_string(cups[index_zero].next);
-		index_zero = cups[index_zero].next;
-	}
+	unsigned long solution;
+	solution = cups[1].next * cups[cups[1].next].next;
 	
 	return solution;
 }
